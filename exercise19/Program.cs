fb9d838cc7f99611ba80266a136b5aa27eee14cc
OLGA <ochefranova@yandex.ru>
2022-11-22 13:22:49 +0500

    задача 19

--- exercise19/Program.cs
+++ exercise19/Program.cs
@@ -2,10 +2,11 @@
 // не является ли оно палиндромом.
 
 Console.Clear();
-int[]array = {25852};
-if (array[0]==array[4] && array[1]==array[3])
+int[] array = {1, 5, 5, 5, 1 };
+
+if(array[0] == array[4] && array[1] == array[3])
 {
-    Console.WriteLine("Палиндром!");
+    Console.WriteLine("палиндром!");
 }
 else
 {
